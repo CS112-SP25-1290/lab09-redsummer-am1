c12e5a60fd2d57f6c4b3ff5f058eb07b07365c64
aj <ajm024717@gmail.com>
2025-04-17 14:57:29 -0700

    Done!

--- src/main/java/cs112/lab09/controllers/CityController.java
+++ src/main/java/cs112/lab09/controllers/CityController.java
@@ -5,11 +5,50 @@ import cs112.lab09.RedSummer;
 import cs112.lab09.models.RevisedHistoricalEvent;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.event.Event;
+import javafx.scene.Scene;
+
+import java.io.IOException;
 
 public class CityController {
+
+    @FXML
+    ImageView cityImageView;
+    @FXML
+    Label dateLabel;
+    @FXML
+    Label cityLabel;
+    @FXML
+    Label descriptionLabel;
+    @FXML
+    Label revisedLabel;
+    @FXML
+    Hyperlink sourceLink;
+
+    public void handleCloseButton()
+    {
+        Stage stage = (Stage) cityImageView.getScene().getWindow();
+        stage.close();
+    }
+
+    public void initData(Constants.Event event)
+    {
+        String[] data = Constants.HISTORICAL_DATA[event.ordinal()];
+        RevisedHistoricalEvent revisedEvent = new RevisedHistoricalEvent(data);
+        cityImageView.setImage(revisedEvent.getImage());
+        cityLabel.setText(revisedEvent.getLocation());
+        dateLabel.setText(revisedEvent.getEventDay().toString());
+        descriptionLabel.setText(revisedEvent.getDescription());
+        revisedLabel.setText(revisedEvent.getRevisedDescription());
+        sourceLink.setText(revisedEvent.getCitation());
+
+    }
 
 }
